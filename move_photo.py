c31db61c8d7ad11b06fd0b88df3660be880b8007
1803031 <37138507+work-nchc@users.noreply.github.com>
2020-02-12 15:36:06 +0800

    Add files via upload

--- move_photo.py
+++ move_photo.py
@@ -1,36 +1,31 @@
-from os import walk, mkdir
+from glob import glob
+from os import mkdir
 from time import strftime, sleep
 from shutil import move
 from PIL import Image
 
-fldr = 'D:/SolidMemory_Face_Rig/Shot_00/'
-fldr2 = 'C:/Users/admin/Pictures/digiCamControl/Session1/'
+src = (
+    'D:/SolidMemory_Face_Rig/Shot_00/*.jpg',
+    'C:/Users/admin/Pictures/digiCamControl/Session1/*.jpg',
+)
 dst = 'Z:/{}/'
 name = '{}.jpg'
 tag_sn = 42033
 sec_sleep = 5
+
 num_old = 0
 while True:
-    try:
-        imgs = next(walk(fldr))[2]
-    except StopIteration:
-        imgs = []
+    imgs = sum(map(glob, src), [])
     if imgs and len(imgs) == num_old:
-        sleep(sec_sleep)
         dst_now = dst.format(strftime('%Y_%m_%d_%H_%M_%S'))
         try:
             mkdir(dst_now)
         except FileExistsError:
             pass
-        {move(fldr + i,
-              dst_now + name.format(Image.open(fldr + i).getexif()[tag_sn]))
-         for i in imgs}
-        try:
-            imgs2 = next(walk(fldr2))[2]
-        except StopIteration:
-            imgs2 = []
-        {move(fldr2 + i,
-              dst_now + name.format(Image.open(fldr2 + i).getexif()[tag_sn]))
-         for i in imgs2}
+        for i in imgs:
+            try:
+                move(i, dst_now + name.format(Image.open(i).getexif()[tag_sn]))
+            except OSError:
+                pass
     num_old = len(imgs)
     sleep(sec_sleep)
